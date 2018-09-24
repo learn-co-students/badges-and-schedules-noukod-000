c5dd69ef6e4614b6d8afbfb84367219c36e527b5
Saint-Jean Germila <germila@noukod.com>
2018-09-24 15:59:21 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,40 +1,28 @@
-def conference_badges(  name, room_index = 1, badges_and_room_assignments)
+def batch_badge_creator(array)
+  array_of_messages = []
+  array.each do |member|
+    array_of_messages.push badge_maker(member)
+  end
+  array_of_messages
+end
 
-                     badge_maker =[ "Hello, my name is #{name}",
-                             "Hello, my name is #{name}",
-                              "Hello, my name is #{name}",
-                              "Hello, my name is #{name}",
-                              "Hello, my name is #{name}",
-                              "Hello, my name is #{name}",
-                              "Hello, my name is #{name}",
-                            ]
-                          end
-                  assign_rooms = [
-                          "Hello, #{name}! You'll be assigned to room #{room_index}!",
-                          "Hello, #{name}! You'll be assigned to room  #{room_index}!",
-                          "Hello, #{name}! You'll be assigned to room #{room_index}!",
-                          "Hello,#{name}! You'll be assigned to room #{room_index}!",
-                          "Hello, #{name}! You'll be assigned to room #{room_index}!",
-                          "Hello, #{name}! You'll be assigned to room #{room_index}!",
-                          "Hello, #{name}! You'll be assigned to room #{room_index}!"
-                          ]
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
 
+def assign_rooms(attendees)
+  assignments = []
+  attendees.each_with_index do |member, index|
+     assignments.push("Hello, #{member}! You'll be assigned to room #{index+1}!")
+  end
+  assignments
+end
 
-    badges_and_room_assignments = [
-                                   "Hello, my name is #{name}",
-                                   "Hello, my name is #{name}",
-                                   "Hello, my name is #{name}",
-                                   "Hello, my name is #{name}",
-                                   "Hello, my name is #{name}",
-                                  "Hello, my name is #{name}",
-                                   "Hello, my name is #{name}",
-                                 "Hello, #{name}! You'll be assigned to #{room}!",
-                                   "Hello, #{name}! You'll be assigned to #{room}!",
-                                   "Hello, #{name}! You'll be assigned to #{room}!",
-                                    "Hello,#{name}! You'll be assigned to #{room}!",
-                                    "Hello, #{name}! You'll be assigned to #{room}!",
-                                    "Hello, #{name}! You'll be assigned to #{room}!",
-                                    "Hello, #{name}! You'll be assigned to #{room}!"
-                                     ]
-      return bagdes_and_room_assignments
-              end# Write your code here.
+def printer(attendees)
+  batch_badge_creator(attendees).each do |result|
+    puts result
+  end
+  assign_rooms(attendees).each do | attendee |
+    puts attendee
+  end
+end
