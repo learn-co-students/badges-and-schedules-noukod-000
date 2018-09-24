197ba04664c545c43dde2bb39b3f4556db4d4fc8
Saint-Jean Germila <germila@noukod.com>
2018-09-24 15:56:48 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+def conference_badges(  name, room_index = 1, badges_and_room_assignments)
+
+                     badge_maker =[ "Hello, my name is #{name}",
+                             "Hello, my name is #{name}",
+                              "Hello, my name is #{name}",
+                              "Hello, my name is #{name}",
+                              "Hello, my name is #{name}",
+                              "Hello, my name is #{name}",
+                              "Hello, my name is #{name}",
+                            ]
+                          end
+                  assign_rooms = [
+                          "Hello, #{name}! You'll be assigned to room #{room_index}!",
+                          "Hello, #{name}! You'll be assigned to room  #{room_index}!",
+                          "Hello, #{name}! You'll be assigned to room #{room_index}!",
+                          "Hello,#{name}! You'll be assigned to room #{room_index}!",
+                          "Hello, #{name}! You'll be assigned to room #{room_index}!",
+                          "Hello, #{name}! You'll be assigned to room #{room_index}!",
+                          "Hello, #{name}! You'll be assigned to room #{room_index}!"
+                          ]
+
+
+    badges_and_room_assignments = [
+                                   "Hello, my name is #{name}",
+                                   "Hello, my name is #{name}",
+                                   "Hello, my name is #{name}",
+                                   "Hello, my name is #{name}",
+                                   "Hello, my name is #{name}",
+                                  "Hello, my name is #{name}",
+                                   "Hello, my name is #{name}",
+                                 "Hello, #{name}! You'll be assigned to #{room}!",
+                                   "Hello, #{name}! You'll be assigned to #{room}!",
+                                   "Hello, #{name}! You'll be assigned to #{room}!",
+                                    "Hello,#{name}! You'll be assigned to #{room}!",
+                                    "Hello, #{name}! You'll be assigned to #{room}!",
+                                    "Hello, #{name}! You'll be assigned to #{room}!",
+                                    "Hello, #{name}! You'll be assigned to #{room}!"
+                                     ]
+      return bagdes_and_room_assignments
+              end# Write your code here.
