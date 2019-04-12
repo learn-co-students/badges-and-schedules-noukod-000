9abc9829ca9c77a4c94c1d83ebfda2c571135a54
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-12 14:22:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(name_array)
+  new_array = []
+  name_array.each do |speaker|
+    new_array << badge_maker(speaker)
+    end
+    return new_array
+end 
+
+def assign_rooms(name)
+  room = 1
+  newlist = []
+  name.each do |speaker|
+    newlist << "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    room += 1 
+  end
+      return newlist
+end
+
+
+def printer(name)
+  batch_badge_creator(name).each do |el|
+    puts el
+  end 
+  assign_rooms(name).each do |el2|
+    puts el2
+  end
+end
