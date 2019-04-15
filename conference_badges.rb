68680a3bd5341a12be5b8b0518c5461737ceae37
James Declerus <james.declerus@esih.edu>
2019-04-15 16:36:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,41 @@
-# Write your code here.
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(array)
+  n_array = []
+  array.each do |name|
+    n_array.push("Hello, my name is #{name}.")
+  end
+  return n_array
+end
+
+# batch_badge_creator(people)
+
+
+
+def assign_rooms(array)
+  n_array = []
+  counter = 1
+  array.each do |name|
+    n_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return n_array
+end
+
+# assign_rooms(people)
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end
+
+  assign_rooms(array).each do |id|
+    puts id
+  end
+end
