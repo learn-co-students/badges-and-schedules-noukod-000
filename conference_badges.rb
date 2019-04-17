7820060279dad4c8f0b0be57a164bba7303f4c51
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-17 18:13:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker bm
+  "Hello, my name is #{bm}."
+end
+
+def batch_badge_creator bc
+  arrBadge = []
+  bc.each do |nen|
+   arrBadge << badge_maker(nen) 
+  end
+  arrBadge
+end
+
+def assign_rooms attendees
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer att
+  batch_badge_creator(att).each do |badge|
+    puts badge
+  end
+  assign_rooms(att).each do |assignment|
+    puts assignment
+  end
+end
+
