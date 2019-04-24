b9041b293bd48e3eb9a8572ad95c6e2de60f36af
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-24 19:15:08 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,8 @@ end
 
 def batch_badge_creator(speakers)
 
+  # returns an array of badge messages, using badge_maker
+
   badge_messages = []
 
   speakers.each do |speaker|
@@ -22,7 +24,13 @@ def batch_badge_creator(speakers)
 
 end
 
+
+
 def assign_rooms(speakers)
+
+  # assigns each speaker to a room, rooms 1-7.
+
+  # return a list of room assignments in the form of: "Hello____! You'll be assigned to room ___!"
 
   room_number = 1
 
@@ -43,6 +51,8 @@ end
 
 
 def printer(speakers)
+
+  # outputs the results of batch_badge_creator, and assign_rooms
 
   badge_messages = batch_badge_creator(speakers)
 
